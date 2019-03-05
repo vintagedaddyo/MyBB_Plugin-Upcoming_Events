bdb7ae8a552bf17dc1231f0dfd62b8fc8f9692ab
Vintagedaddyo <vintagedaddyo@gmail.com>
2019-03-05 01:32:18 -0800

    further localization add espanol

---
+++ Upload/inc/languages/espanol/admin/upcoming_events.lang.php
@@ -0,0 +1,65 @@
+<?php
+/***************************************************************************
+ *
+ *   Upcoming Events for MyBB
+ *   Copyright: © 2011 by Christopher Lorentz
+ *   
+ *   Website: http://lorus.org/
+ *   Author: Lorus
+ *   Updated by: Vintagedaddyo
+ *   Website: http://community.mybb.com/user-6029.html
+ *
+ *   
+ *   Last modified: 03/04/2019 by Vintagedaddyo
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+// Plugin info
+
+$l['upcoming_events_PName'] = 'Proximos Eventos';
+$l['upcoming_events_PDesc'] = 'Muestra los proximos eventos en el índice del foro y en la pagina del portal.';
+$l['upcoming_events_PWeb'] = 'http://community.mybb.com/user-6029.html';
+$l['upcoming_events_PAuth'] = 'Lorus & Actualizado por Vintagedaddyo';
+$l['upcoming_events_PAuthSite'] = 'http://community.mybb.com/user-6029.html';
+$l['upcoming_events_PVer'] = '1.32';
+$l['upcoming_events_PGUID'] = 'b364c7af6f5fc2440c725fb91197bbd5';
+$l['upcoming_events_PCompat'] = '18*';
+
+// Plugin Settings
+
+$l['upcoming_events'] = "Proximos Eventos";
+$l['upcoming_events_desc'] = "Muestra los proximos eventos en el indice del foro y en la pagina del portal.";
+
+$l['setting_group_upcoming_events'] = $l['upcoming_events'];
+$l['setting_group_upcoming_events_desc'] = "Aqui puedes configurar el plugin de Proximos Eventos.";
+
+$l['setting_upcoming_events_timerange'] = "Mostrar temporizador";
+$l['setting_upcoming_events_timerange_desc'] = "How many days in advance should events are to be displayed from the calendar?";
+
+$l['setting_upcoming_events_maxdisplay'] = "Eventos maximos visibles";
+$l['setting_upcoming_events_maxdisplay_desc'] = "Cuantos eventos deben ser visibles (max.)?";
+
+$l['setting_upcoming_events_showindex'] = "Mostrar en el indice";
+$l['setting_upcoming_events_showindex_desc'] = "Deshabilitados proximos eventos en la pagina indice del foro.";
+
+$l['setting_upcoming_events_showportal'] = "Mostrar en el portal";
+$l['setting_upcoming_events_showportal_desc'] = "Deshabilitados proximos eventos en la pagina del portal.";
+
+?>
---
+++ Upload/inc/languages/espanol/upcoming_events.lang.php
@@ -0,0 +1,44 @@
+<?php
+/***************************************************************************
+ *
+ *   Upcoming Events for MyBB
+ *   Copyright: © 2011 by Christopher Lorentz
+ *   
+ *   Website: http://community.mybb.com/user-6029.html
+ *   Author: Lorus
+ *   Updated by: Vintagedaddyo
+ *   Website: http://community.mybb.com/user-6029.html
+ *
+ *   
+ *   Last modified: 03/04/2019 by Vintagedaddyo
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+// Plugin Frontend 
+
+$l['upcoming_events'] = "Proximos eventos </strong> <i>(proximo {1} en {2} dias)</i><strong>";
+$l['upcoming_events_portal'] = "Proximos Eventos";
+$l['upcoming_events_no_events'] = "No hay proximos eventos";
+$l['upcoming_events_created'] = ' <i>publicado por {1}</i>';
+$l['upcoming_events_eventline'] = '{1} - {2} ({3} - {4})';
+$l['upcoming_events_eventline_day'] = '{1} - {2} (todo el dia)';
+$l['upcoming_events_today'] = "Hoy";
+
+?>

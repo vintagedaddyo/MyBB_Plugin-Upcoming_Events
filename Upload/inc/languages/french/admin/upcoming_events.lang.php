dea2c48c5b0d025fc8388462bcac4ad67b677844
Vintagedaddyo <vintagedaddyo@gmail.com>
2019-03-05 02:03:07 -0800

    Further localization french

---
+++ Upload/inc/languages/french/admin/upcoming_events.lang.php
@@ -0,0 +1,65 @@
+<?php
+/***************************************************************************
+ *
+ *   Upcoming Events for MyBB
+ *   Copyright: © 2011 by Christopher Lorentz
+ *   
+ *   Website: http://lorus.org/
+ *   Author: Lorus
+ *   Updated by: Vintagedaddyo
+ *   Website: http://community.mybb.com/user-6029.html
+ *
+ *   
+ *   Last modified: 03/04/2019 by Vintagedaddyo
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+// Plugin info
+
+$l['upcoming_events_PName'] = 'evenements a venir';
+$l['upcoming_events_PDesc'] = 'Affiche les evenements a venir dans l index du forum et sur la page du portail.';
+$l['upcoming_events_PWeb'] = 'http://community.mybb.com/user-6029.html';
+$l['upcoming_events_PAuth'] = 'Lorus & updated by Vintagedaddyo';
+$l['upcoming_events_PAuthSite'] = 'http://community.mybb.com/user-6029.html';
+$l['upcoming_events_PVer'] = '1.32';
+$l['upcoming_events_PGUID'] = 'b364c7af6f5fc2440c725fb91197bbd5';
+$l['upcoming_events_PCompat'] = '18*';
+
+// Plugin Settings
+
+$l['upcoming_events'] = "evenements a venir";
+$l['upcoming_events_desc'] = "Affiche les evenements e venir dans l index du forum et sur la page du portail.";
+
+$l['setting_group_upcoming_events'] = $l['upcoming_events'];
+$l['setting_group_upcoming_events_desc'] = "Ici vous pouvez configurer le plug-in Evenements àavenir.";
+
+$l['setting_upcoming_events_timerange'] = "Afficher la plage horaire";
+$l['setting_upcoming_events_timerange_desc'] = "Combien de jours a l avance les evenements doivent-ils etre affiches dans le calendrier?";
+
+$l['setting_upcoming_events_maxdisplay'] = "Nombre maximum d evenements visibles";
+$l['setting_upcoming_events_maxdisplay_desc'] = "Combien d evenements doivent etre visibles (max.)?";
+
+$l['setting_upcoming_events_showindex'] = "Afficher sur l index";
+$l['setting_upcoming_events_showindex_desc'] = "Desactive les evenements a venir sur la page d index du forum.";
+
+$l['setting_upcoming_events_showportal'] = "Afficher sur le portail";
+$l['setting_upcoming_events_showportal_desc'] = "Événements a venir désactives sur la page du portail.";
+
+?>
---
+++ Upload/inc/languages/french/upcoming_events.lang.php
@@ -0,0 +1,44 @@
+<?php
+/***************************************************************************
+ *
+ *   Upcoming Events for MyBB
+ *   Copyright: © 2011 by Christopher Lorentz
+ *   
+ *   Website: http://lorus.org/
+ *   Author: Lorus
+ *   Updated by: Vintagedaddyo
+ *   Website: http://community.mybb.com/user-6029.html
+ *
+ *   
+ *   Last modified: 03/04/2019 by Vintagedaddyo
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+// Plugin Frontend 
+
+$l['upcoming_events'] = "Evenements a venir </strong> <i>({1} dans les {2} jours suivants)</i> <strong>";
+$l['upcoming_events_portal'] = "evenements a venir";
+$l['upcoming_events_no_events'] = "Prochains evenementsAucun evenement a venir";
+$l['upcoming_events_created'] = ' <i>poste par {1}</i>';
+$l['upcoming_events_eventline'] = '{1} - {2} ({3} - {4})';
+$l['upcoming_events_eventline_day'] = '{1} - {2} (toute la journee)';
+$l['upcoming_events_today'] = "Aujourd hui";
+
+?>

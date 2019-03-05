57a3ce4fceacc6480c68f3705b6d850031c020af
Vintagedaddyo <vintagedaddyo@gmail.com>
2019-03-05 13:30:18 -0800

    Begin further localization italiano

---
+++ Upload/inc/languages/italiano/admin/upcoming_events.lang.php
@@ -0,0 +1,65 @@
+<?php
+/***************************************************************************
+ *
+ *   Upcoming Events for MyBB
+ *   Copyright: © 2011 by Christopher Lorentz
+ *   
+ *   Website: http://lorus.org/
+ *   Author: Lorus
+ *   Updated by: Vintagedaddyo
+ *   Website: http://community.mybb.com/user-6029.html
+ *
+ *   
+ *   Last modified: 03/04/2019 by Vintagedaddyo
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+// Plugin info
+
+$l['upcoming_events_PName'] = 'Upcoming Events';
+$l['upcoming_events_PDesc'] = 'Shows upcoming events at the forum index and at the portal page.';
+$l['upcoming_events_PWeb'] = 'http://community.mybb.com/user-6029.html';
+$l['upcoming_events_PAuth'] = 'Lorus & updated by Vintagedaddyo';
+$l['upcoming_events_PAuthSite'] = 'http://community.mybb.com/user-6029.html';
+$l['upcoming_events_PVer'] = '1.32';
+$l['upcoming_events_PGUID'] = 'b364c7af6f5fc2440c725fb91197bbd5';
+$l['upcoming_events_PCompat'] = '18*';
+
+// Plugin Settings
+
+$l['upcoming_events'] = "Upcoming Events";
+$l['upcoming_events_desc'] = "Shows upcoming events at the forum index and at the portal page.";
+
+$l['setting_group_upcoming_events'] = $l['upcoming_events'];
+$l['setting_group_upcoming_events_desc'] = "Here you can configure Upcoming Events plugin.";
+
+$l['setting_upcoming_events_timerange'] = "Display timerange";
+$l['setting_upcoming_events_timerange_desc'] = "How many days in advance should events are to be displayed from the calendar?";
+
+$l['setting_upcoming_events_maxdisplay'] = "Maximum Events visible";
+$l['setting_upcoming_events_maxdisplay_desc'] = "How many events should be visible (max.)?";
+
+$l['setting_upcoming_events_showindex'] = "Show on Index";
+$l['setting_upcoming_events_showindex_desc'] = "Disables upcoming events on forum index page.";
+
+$l['setting_upcoming_events_showportal'] = "Show on Portal";
+$l['setting_upcoming_events_showportal_desc'] = "Disables upcoming events on portal page.";
+
+?>
